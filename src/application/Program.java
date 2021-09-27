cdfdb17411d0cb250cb37a1f765a493905763202
Junior <cleveranalista@gmail.com>
2021-09-27 14:06:13 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,41 +16,44 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		
-		  System.out.println("Qual a ID do funcionário que deseja encontrar:"); int id
-		  = sc.nextInt(); Seller seller = sellerDao.findById(id);
-		  System.out.println(seller);
-		  
-		  System.out.println("\n===Teste 2: Seller findByDepartment ====");
-		  
-		  System.out.println("Digite o número do Departamento:"); int idDep =
-		  sc.nextInt(); Department department = new Department(idDep, null);
-		  List<Seller> list = sellerDao.findByDepartment(department);
-		  
-		  for (Seller obj : list ) { System.out.println(obj); }
-		  
-		  System.out.println("\n\n=======TESTE 3: Seller FindAll ==========="); list =
-		  sellerDao.findAll();
-		  
-		  for (Seller obj : list ) { System.out.println(obj); }
-		  
-		  System.out.println("\n\n=======TESTE 4: Seller INSERT ===========");
-		  System.out.println("Digite o nome do novo Vendedor:"); String nameVendedor =
-		  sc.next(); Seller newSeller = new Seller(null, nameVendedor,
-		  "RenatoCarlos@gmail.com", new Date(), 4000.0, department);
-		  sellerDao.insert(newSeller); System.out.println("Inserted! New id = " +
-		  newSeller.getId());
-		 
-
-		System.out.println("\n\n=======TESTE 5: Seller UPDATE ===========");
-		System.out.println("Digite a ID do vendedor");
-		int idv = sc.nextInt();
-		sc.nextLine();
-		seller = sellerDao.findById(idv);
-		System.out.println("Digite o Nome do Vendedor:");
-		String nameV = sc.nextLine();
-		seller.setName(nameV);
-		sellerDao.update(seller);
-
+		/*
+		 * System.out.println("Qual a ID do funcionário que deseja encontrar:"); int id
+		 * = sc.nextInt(); Seller seller = sellerDao.findById(id);
+		 * System.out.println(seller);
+		 * 
+		 * System.out.println("\n===Teste 2: Seller findByDepartment ====");
+		 * 
+		 * System.out.println("Digite o número do Departamento:"); int idDep =
+		 * sc.nextInt(); Department department = new Department(idDep, null);
+		 * List<Seller> list = sellerDao.findByDepartment(department);
+		 * 
+		 * for (Seller obj : list ) { System.out.println(obj); }
+		 * 
+		 * System.out.println("\n\n=======TESTE 3: Seller FindAll ==========="); list =
+		 * sellerDao.findAll();
+		 * 
+		 * for (Seller obj : list ) { System.out.println(obj); }
+		 * 
+		 * System.out.println("\n\n=======TESTE 4: Seller INSERT ===========");
+		 * System.out.println("Digite o nome do novo Vendedor:"); String nameVendedor =
+		 * sc.next(); Seller newSeller = new Seller(null, nameVendedor,
+		 * "RenatoCarlos@gmail.com", new Date(), 4000.0, department);
+		 * sellerDao.insert(newSeller); System.out.println("Inserted! New id = " +
+		 * newSeller.getId());
+		 * 
+		 * 
+		 * System.out.println("\n\n=======TESTE 5: Seller UPDATE ===========");
+		 * System.out.println("Digite a ID do vendedor"); int idv = sc.nextInt();
+		 * sc.nextLine(); seller = sellerDao.findById(idv);
+		 * System.out.println("Digite o Nome do Vendedor:"); String nameV =
+		 * sc.nextLine(); seller.setName(nameV); sellerDao.update(seller);
+		 */
+		
+		System.out.println("\n\n=======TESTE 6: Seller DELETE ===========");
+		System.out.println("Enter id for delete byID:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		
 		sc.close();
 
 	}
